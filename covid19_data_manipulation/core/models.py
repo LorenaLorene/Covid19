94fa2183684cb918a779a2212c564336f09fca13
Lorena <Lorenija@gmail.com>
2020-05-05 16:58:06 +0100

    creating initial data structure

--- covid19_data_manipulation/core/models.py
+++ covid19_data_manipulation/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class CountryRegion(models.Model):
+    country = models.CharField(max_length=30)
+    province_state = models.CharField(max_length=30)
+    population = models.DecimalField(max_digits=8, decimal_places=2)
+
+
+class DailyStatistics(models.Model):
+    date = models.DateTimeField(auto_now_add=False)
+    confirmed = models.IntegerField()
+    deaths = models.IntegerField()
+    recovered = models.IntegerField()
+    active = models.IntegerField()
+    country_region = models.OneToOneField(CountryRegion, on_delete=models.CASCADE, primary_key=True)
