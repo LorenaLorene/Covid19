436e0bfd39281b0c1248a862c39bf7cfadfb48f9
Lorena <Lorenija@gmail.com>
2020-05-06 10:53:42 +0100

    alter tables

--- covid19_data_manipulation/core/models.py
+++ covid19_data_manipulation/core/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class CountryRegion(models.Model):
     country = models.CharField(max_length=30)
-    province_state = models.CharField(max_length=30)
-    population = models.DecimalField(max_digits=8, decimal_places=2)
+    province_state = models.CharField(max_length=30, null=True, blank=True)
+    population = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
 
 
 class DailyStatistics(models.Model):
     date = models.DateTimeField(auto_now_add=False)
-    confirmed = models.IntegerField()
-    deaths = models.IntegerField()
-    recovered = models.IntegerField()
-    active = models.IntegerField()
+    confirmed = models.IntegerField(null=True, blank=True)
+    deaths = models.IntegerField(null=True, blank=True)
+    recovered = models.IntegerField(null=True, blank=True)
+    active = models.IntegerField(null=True, blank=True)
     country_region = models.OneToOneField(CountryRegion, on_delete=models.CASCADE, primary_key=True)
